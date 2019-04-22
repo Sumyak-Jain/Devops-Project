8f687acbad0e2659f2f1ec1660badca5beda70ae
Vanshika <vanshikaaa1009@gmail.com>
2019-04-23 00:22:09 +0530

    updated

--- part1.cpp
+++ part1.cpp
@@ -1,5 +1,43 @@
 #include <iostream>
 using namespace std;
+void binary search()
+{
+	int count, i, arr[30], num, first, last, middle;
+	cout<<"how many elements would you like to enter?:";
+        cin>>count;
+
+	for (i=0; i<count; i++)
+	{
+		cout<<"Enter number "<<(i+1)<<": ";
+                cin>>arr[i];
+	}
+	cout<<"Enter the number that you want to search:";
+        cin>>num;
+	first = 0;
+	last = count-1;
+	middle = (first+last)/2;
+	while (first <= last)
+	{
+	   if(arr[middle] < num)
+	   {
+		first = middle + 1;
+
+	   }
+	   else if(arr[middle] == num)
+	   {
+		cout<<num<<" found in the array at the location "<<middle+1<<"\n";
+                break;
+           }
+           else {
+                last = middle - 1;
+           }
+           middle = (first + last)/2;
+        }
+        if(first > last)
+	{
+	   cout<<num<<" not found in the array";
+	}
+}
 int factorial()
 {
    int n = 6, fact = 1, i;    
@@ -122,5 +160,7 @@ int main ()
     factorial();
     lcm();
     getchar();
+    binarysearch();
+
     return 0;
 }
